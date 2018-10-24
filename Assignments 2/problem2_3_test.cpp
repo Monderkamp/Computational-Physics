355a444777831bd8713417a7e4f767f720490733
Monderkamp <paul.monderkamp@hhu.de>
2018-10-24 23:40:21 +0200

    123

--- Assignments 2/problem2_3_test.cpp
+++ Assignments 2/problem2_3_test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <math.h>
-#include <fstream> 
+ 
 
 using namespace std;
 
@@ -12,19 +12,21 @@ return((35.0*pow(x,4.0)+0.0*pow(x,3.0)-30.0*pow(x,2.0)-0.0*pow(x,1.0)+3.0)/8.0);
 
 double fstrich(double x)
 {
-return((140.0*pow(x,3.0)+0.0*pow(x,2.0)-60.0*pow(x,1.0)-0.0)/8);	
+return(140.0*pow(x,3.0)+0.0*pow(x,2.0)-60.0*pow(x,1.0)-0.0);	
 }
+
 
 
 int main()
 {
-ofstream out("root1")	
-
+double dx = 0.01;
 double epsilon = 1.0e-12;
 cout << "tolerance in f(x) if what is equal to zero = " << epsilon << endl;
 
+for (double x = -1.0; x <= 1.0; x += dx)
+	{
 		int counter = 0;
-		double x0 = -0.7;		
+		double x0 = x;		
 		cout << "x0 = " << x0;	
 
 		while (fabs(f(x0)) > epsilon)
@@ -36,8 +38,7 @@ cout << "tolerance in f(x) if what is equal to zero = " << epsilon << endl;
 		
 		cout << " root equal to " << x0;
 		cout << " counter = " << counter << endl;
-out.close();
+	}
 getchar();
 return 0;
 }
-
