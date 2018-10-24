3cec741bd109a6e5250589679eada8d32b72ea4e
Monderkamp <paul.monderkamp@hhu.de>
2018-10-24 23:56:00 +0200

    2410

--- Assignments 2/problem2_3_test.cpp
+++ Assignments 2/problem2_3_test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <math.h>
  
-
+#include <fstream>
 using namespace std;
 
 double f(double x)
@@ -12,22 +12,25 @@ return((35.0*pow(x,4.0)+0.0*pow(x,3.0)-30.0*pow(x,2.0)-0.0*pow(x,1.0)+3.0)/8.0);
 
 double fstrich(double x)
 {
-return(140.0*pow(x,3.0)+0.0*pow(x,2.0)-60.0*pow(x,1.0)-0.0);	
+return((140.0*pow(x,3.0)+0.0*pow(x,2.0)-60.0*pow(x,1.0)-0.0)/8.0);	
 }
 
 
 
 int main()
 {
+
+ofstream out("testdata");
+
 double dx = 0.01;
 double epsilon = 1.0e-12;
-cout << "tolerance in f(x) if what is equal to zero = " << epsilon << endl;
+out << "tolerance in f(x) if what is equal to zero = " << epsilon << endl;
 
 for (double x = -1.0; x <= 1.0; x += dx)
 	{
 		int counter = 0;
 		double x0 = x;		
-		cout << "x0 = " << x0;	
+		out << "x0 = " << x0;	
 
 		while (fabs(f(x0)) > epsilon)
 			{
@@ -36,9 +39,10 @@ for (double x = -1.0; x <= 1.0; x += dx)
 			counter++;
 			}	
 		
-		cout << " root equal to " << x0;
-		cout << " counter = " << counter << endl;
+		out << " root equal to " << x0;
+		out << " counter = " << counter << endl;
 	}
+out.close();
 getchar();
 return 0;
 }
