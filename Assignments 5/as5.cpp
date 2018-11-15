dd50061f1748cad13e330eb67ffd61537b928eec
Monderkamp <paul.monderkamp@hhu.de>
2018-11-15 17:42:39 +0100

    forces

--- Assignments 5/as5.cpp
+++ Assignments 5/as5.cpp
@@ -7,7 +7,7 @@
 #include <string>
 
 using namespace std;
-
+const double cutoff = pow(2.0,1.0/6.0);
 
 #include "readin.cpp"
 typedef vector<double> vouble; 
@@ -38,6 +38,57 @@ class particle
     double x,y,vx,vy;
   };
 
+vouble f_qp(particle p, particle q,int i) // force on particle q from particle p
+  {
+    vouble force(2);
+    force[0] = 0.0;
+    force[1] = 0.0;
+
+    double dx = q.get_x() - p.get_x();
+    double dy = q.get_y() - p.get_y();
+    if (dx > 7.0) {dx -= 14.0;}
+    if (dx < 7.0) {dx += 14.0;}
+    if (dy > 7.0) {dy -= 14.0;}
+    if (dy < 7.0) {dy += 14.0;}
+
+    double dr = sqrt(dx*dx+dy*dy);
+    if (dr <= cutoff) 
+      {
+        force[0] += (dx/dr)*(48.0*pow(dr,-13.0)-24.0*pow(dr,-7.0));
+        force[1] += (dy/dr)*(48.0*pow(dr,-13.0)-24.0*pow(dr,-7.0));
+      }
+
+    return force;
+  }
+
+vouble f_i(particle *p,int i, int N) // force on particle with index i
+  {
+    vouble force(2);
+    force[0] = 0.0;
+    force[1] = 0.0;
+
+    for (int j=0; j<N;j++)
+      {
+        if (j != i)
+	  {
+            double dx = p[i].get_x() - p[j].get_x();
+            double dy = p[i].get_y() - p[j].get_y();
+    	    if (dx > 7.0) {dx -= 14.0;}
+    	    if (dx < 7.0) {dx += 14.0;}
+    	    if (dy > 7.0) {dy -= 14.0;}
+    	    if (dy < 7.0) {dy += 14.0;}
+
+    	    double dr = sqrt(dx*dx+dy*dy);
+    	    if (dr <= cutoff) 
+      	      {
+        	force[0] += (dx/dr)*(48.0*pow(dr,-13.0)-24.0*pow(dr,-7.0));
+        	force[1] += (dy/dr)*(48.0*pow(dr,-13.0)-24.0*pow(dr,-7.0));
+      	      }
+          }
+      }
+    return force;
+  }
+
 int main()
   {
     vouble pos_x = get_column("posdat2.txt",2,3);
@@ -56,6 +107,7 @@ int main()
 
 
     getchar();
+
     return 0;
   }
 
