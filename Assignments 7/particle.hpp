505de11d83d54edb2c5a767803bbc10caaa5718f
Monderkamp <paul.monderkamp@hhu.de>
2018-11-30 23:39:44 +0100

    en cons resolved

---
+++ Assignments 7/particle.hpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include <vector>
+
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+
+typedef vector<double> vouble; 
+
+#ifndef PARTICLE_HPP
+#define PARTICLE_HPP
+
+class particle 
+  {
+    public:
+    particle()
+      {
+        x = 0.0;
+        y = 0.0;
+        vx = 0.0;
+        vy = 0.0;
+      }
+    ~particle(){}
+    
+    void set_x(const double a){x = a;}
+    void set_y(const double a){y = a;}
+    void set_vx(const double a){vx = a;}
+    void set_vy(const double a){vy = a;}
+    
+    double get_x(){return x;}
+    double get_y(){return y;}
+    double get_vx(){return vx;}
+    double get_vy(){return vy;}
+    //friend vouble f_i(particle *p,int i, int N); // force on particle with index i
+
+    private:
+    double x,y,vx,vy;
+  };
+
+#endif
