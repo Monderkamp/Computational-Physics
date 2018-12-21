4601cbb613104e608c2109776b135d159860d5f2
Monderkamp <paul.monderkamp@hhu.de>
2018-12-21 21:46:30 +0100

    done except description

--- Assignments 10/WCA_MC.cpp
+++ Assignments 10/WCA_MC.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <fstream>
 #include <vector>
+#include <string>
 
 #include "MD_functions.hpp"
 #include "particle.hpp"
@@ -19,27 +20,53 @@ int main()
         const double cutoff = pow(2.0,1.0/6.0);
         double delta = 0.1;
         const int N = 144;
-        const int Nsweeps = 1e4;
+        const int Nsweeps = 1e2;
+        const int Nbins = 200;
+
         const int Nsteps = N*Nsweeps;
+        int acceptence = 0;
 
         particle p[N];
         particle p0[N];
-        vouble x_pos(N),y_pos(N);
+        vouble x_pos(N);
+        vouble y_pos(N);
         x_pos = get_column("init_conf.txt",1,5);
         y_pos = get_column("init_conf.txt",2,5);
-        for (int n=0;n>N;n++)
+
+        for (int n=0;n<N;n++)
             {
                 p[n].set_x(x_pos[n]);
                 p[n].set_y(y_pos[n]);
                 p0[n].set_x(x_pos[n]);
                 p0[n].set_y(y_pos[n]);
+
+                //cout << p[n].get_x() << "    " << p[n].get_y() << endl;
             }
+        ofstream msd_out("msd_Nsweeps=" + to_string(Nsweeps) + "_delta=" + to_string(delta) + ".txt");
         
 
-        for (int k=0;k>Nsteps;k++)
+        vouble g(Nbins);
+        vouble g_imd(Nbins);
+        //vouble rdf(particle* p, int N, const double BoxL, const int Nbins);
+        for (int k=0;k<Nsteps;k++)
             {
+
+                if (k % (Nsteps/1000) == 0)
+                    {
+                        //cout << (double) k/Nsteps << endl;
+                        msd_out << k << "    " << MSD(p0,p,N,sideL) << endl;
+
+                        g_imd = rdf(p,N,sideL,Nbins);
+                        for (int n=0;n<Nbins;n++) 
+                            {
+                                g_imd[n]/=1000;
+                                g[n] += g_imd[n];
+                            }
+                    }
                 double V_old = V_pot(p,N,sideL,cutoff);
-                int ce = (int) rnm()*N;
+                //cout << V_old << endl;
+                int ce = rnm()*N;
+                //cout << ce << endl;
                 if (ce == N)
                     {
                     cout << "ce == N" << endl;
@@ -48,20 +75,54 @@ int main()
                 double dx = (rnm()-1.0)*delta;
                 double dy = (rnm()-1.0)*delta;
                 
+                double x_old = p[ce].get_x();
+                double y_old = p[ce].get_y();
+
                 p[ce].set_x(p[ce].get_x()+dx);
                 p[ce].set_y(p[ce].get_y()+dy);
 
+                if (p[ce].get_x() > sideL) {p[ce].set_x(p[ce].get_x()- sideL);}
+                if (p[ce].get_x() < 0.0) {p[ce].set_x(p[ce].get_x()+ sideL);}
+
+                if (p[ce].get_y() > sideL) {p[ce].set_y(p[ce].get_y()- sideL);}
+                if (p[ce].get_y() < 0.0) {p[ce].set_y(p[ce].get_y()+ sideL);}
+
                 double dV = V_pot(p,N,sideL,cutoff)-V_old;
+                //cout << dV << endl;
                 if (dV > 0)
                     {
                         double w0 = exp(-dV);
                         double w = rnm();
                         if (w > w0)
                             {
-                                p[ce].set_x(p[ce].get_x()-dx);
-                                p[ce].set_y(p[ce].get_y()-dy);
+                                p[ce].set_x(x_old);
+                                p[ce].set_y(y_old);
+                                continue;
                             }
                     }
+                acceptence ++;
             }
+        msd_out.close();
+        cout << "fertig!" << endl;
+        cout << "delta = " << delta << ", acceptence rate: " << (double) acceptence/Nsteps << endl;
+
+        ofstream final_out("finalpos_Nsweeps=" + to_string(Nsweeps) + "_delta=" + to_string(delta) + ".txt");
+        for (int n=0;n<N;n++)
+            {
+                final_out << p[n].get_x() << "    " << p[n].get_y() << endl;
+            }
+        final_out.close();
+
+    ofstream g_out("g_Nsweeps=" + to_string(Nsweeps) + "_delta=" + to_string(delta) + ".txt");
+        
+    for (int i=0;i<Nbins;i++)
+        {
+           g_out << i << "    " << (i+0.5)*0.5*sideL/Nbins << "    " << g[i] << endl;
+            //cout << i << "    " << (i+0.5)*0.5*sideL/Nbins << "    " << g[i] << endl;
+        }
+    g_out.close(); 
+
+
+        getchar();
         return 0;
     }
--- Assignments 10/as10_plot.m
+++ Assignments 10/as10_plot.m
@@ -6,8 +6,62 @@ B = importdata('init_conf.txt');
 
 figure
 scatter(B(:,2),B(:,3),'.');
+legend('initial position','location','northeastoutside')
+axis([0 14 0 14])
+daspect([1 1 1])
 
+msd = importdata('msd_Nsweeps=100_delta=0.200000.txt');
+msd(:,1) = msd(:,1)/144;
+figure 
+loglog(msd(:,1),msd(:,2))
+legend('msd','location','southeast')
 
+finalpos = importdata('finalpos_Nsweeps=100_delta=0.200000.txt');
+figure
+scatter(finalpos(:,1),finalpos(:,2),'.');
+legend('final position','location','northeastoutside')
+axis([0 14 0 14])
+daspect([1 1 1])
 
+acc_rate = zeros(4,2);
+
+acc_rate(1,1) = 0.01; %delta
+acc_rate(1,2) = 0.995556; %rate
+
+acc_rate(2,1) = 0.1; %delta
+acc_rate(2,2) =  0.693889; %rate
+
+acc_rate(3,1) = 0.2; %delta
+acc_rate(3,2) = 0.446458; %rate
+
+acc_rate(4,1) = 0.25; %delta
+acc_rate(4,2) = 0.372639; %rate
+
+acc_rate(5,1) = 0.5; %delta
+acc_rate(5,2) = 0.140278; %rate
+
+acc_rate(6,1) = 1; %delta
+acc_rate(6,2) = 0.0417361; %rate
+
+acc_rate(7,1) = 2; %delta
+acc_rate(7,2) = 0.00722222; %rate
+
+acc_rate(8,1) = 3; %delta
+acc_rate(8,2) = 0.00326389; %rate
+
+accrfit = fit(acc_rate(:,1),acc_rate(:,2),'exp1')
+coeffs = coeffvalues(accrfit)
+
+fit = @(x) coeffs(1)*exp(coeffs(2)*x)
+
+figure 
+x = 1:1000;
+x = x*3/1000;
+plot(acc_rate(:,1),acc_rate(:,2),x,fit(x))
+legend('acceptence rate vs delta','exponential fit of acceptence rate vs delta','location','northeast')
+
+G = importdata('g_Nsweeps=100_delta=0.100000.txt')
+figure
+plot(G(:,2),G(:,3))
 
 
